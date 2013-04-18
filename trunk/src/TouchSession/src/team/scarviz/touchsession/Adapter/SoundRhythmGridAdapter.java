b175294fa8f9d92a7d99c2716d9a033fe76fed97
katsuki.nakatani <katsuki.nakatani@53889051-d8cb-3e35-6fa2-57f115644bfa>
2013-04-18 16:03:54 +0000

    拍子編集のボタンスタイル組み込み それに伴うAdapterのイベント変更

--- trunk/src/TouchSession/src/team/scarviz/touchsession/Adapter/SoundRhythmGridAdapter.java
+++ trunk/src/TouchSession/src/team/scarviz/touchsession/Adapter/SoundRhythmGridAdapter.java
@@ -4,22 +4,24 @@ import java.util.List;
 
 import team.scarviz.touchsession.R;
 import team.scarviz.touchsession.Data.SoundRhythmData;
+import team.scarviz.touchsession.Dto.SoundDto;
 import android.content.Context;
-import android.graphics.PorterDuff.Mode;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 
 public class SoundRhythmGridAdapter extends ArrayAdapter<SoundRhythmData> {
 	protected LayoutInflater mInflater;
 	private List<SoundRhythmData> mItems;
+	private SoundDto mSoundDto;
 	ViewHolder viewHolder;
 
 
 	static class ViewHolder{
-		public ImageView ImageRhytmView;
+		public ImageButton ImageRhytmView;
 	}
 
 	/**
@@ -28,13 +30,16 @@ public class SoundRhythmGridAdapter extends ArrayAdapter<SoundRhythmData> {
 	 * @param rowLayoutResourceId	���\�[�XID
 	 * @param items					�A�C�e��
 	 */
-	public SoundRhythmGridAdapter(Context context,List<SoundRhythmData> items) {
+	public SoundRhythmGridAdapter(Context context,List<SoundRhythmData> items,SoundDto sDto) {
 		super(context, 0, items);
 		this.mItems = items;
 		this.mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
+		mSoundDto = sDto;
 	}
 
+	public void setSoundDto(SoundDto sDto){
+		mSoundDto = sDto;
+	}
 
 	/**
 	 * �r���[�̕\���A�r���[�̎擾�����߂��邽�ьĂяo�����
@@ -45,14 +50,26 @@ public class SoundRhythmGridAdapter extends ArrayAdapter<SoundRhythmData> {
 		if (convertView == null) {
 			view = mInflater.inflate(R.layout.sound_edit_view_grid_item, null);
 			viewHolder = new ViewHolder();
-			viewHolder.ImageRhytmView = (ImageView)view.findViewById(R.id.SoundEditViewGridItemImageView);
+			viewHolder.ImageRhytmView = (ImageButton)view.findViewById(R.id.SoundEditViewGridItemImageView);
 			view.setTag(viewHolder);
 		} else {
 			view = convertView;
    		}
 		if(mItems.get(position).getSoundId() >= 0)
-			((ViewHolder)view.getTag()).ImageRhytmView.setColorFilter(mItems.get(position).getFilterColor(),Mode.SRC_ATOP);
+			((ViewHolder)view.getTag()).ImageRhytmView.setBackgroundColor(mItems.get(position).getFilterColor());
+
+		((ViewHolder)view.getTag()).ImageRhytmView.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				   mItems.get(position).setSoundId(mSoundDto.getSoundId());
+				   mItems.get(position).setFilterColor(mSoundDto.getSoundColor());
+				   //adapter.notifyDataSetChanged();
+				   notifyDataSetChanged();
+			}
+		});
 
 		return view;
 	}
+
 }
