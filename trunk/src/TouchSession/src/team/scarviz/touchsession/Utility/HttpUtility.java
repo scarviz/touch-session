976cbb9653b2fb59bdf0a8f8f9423297fc139563
katsuki.nakatani <katsuki.nakatani@53889051-d8cb-3e35-6fa2-57f115644bfa>
2013-04-20 08:49:42 +0000

    レイアウト変更。最終コミット

--- trunk/src/TouchSession/src/team/scarviz/touchsession/Utility/HttpUtility.java
+++ trunk/src/TouchSession/src/team/scarviz/touchsession/Utility/HttpUtility.java
@@ -1,9 +1,17 @@
 package team.scarviz.touchsession.Utility;
 
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -11,10 +19,13 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
 import team.scarviz.touchsession.Dto.CompositionDto;
 import team.scarviz.touchsession.Dto.SoundDto;
 import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
 
 
 public class HttpUtility {
@@ -29,13 +40,22 @@ public class HttpUtility {
 			HttpClient httpclient = new DefaultHttpClient();
 			SoundDto sDto = null;
 			try {
-//					List<NameValuePair> params = new ArrayList<NameValuePair>();
-//					params.add(new BasicNameValuePair("zandaka", StringUtility.toString(zandaka)));
-//					String result = post(httpclient,"url",params,false);
-//				String json = get(httpclient,"url?key=" + result,false);
-				sDto = new SoundDto();
-				sDto.setOwner(1);
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+					String result = post(httpclient,"http://gosrv.scarviz.net:59702/touchsession/nfcsound?nfcdata=" + zandaka,params,false);
+					JSONObject json = new JSONObject(result);
+					String soundId = json.getString("soundid");
+
+					File f = Environment.getExternalStorageDirectory();
+					String path = f.getAbsolutePath() + "/touchsession/" + soundId;
+					if(download(con, "http://gosrv.scarviz.net:59702/touchsession/reqsounddata?soundid=" + soundId,path ));
+					if(!SoundDto.isExsits(con, NumberUtility.toInt(soundId))){
+						sDto = new SoundDto();
+						sDto.setSoundId(NumberUtility.toInt(soundId));
+						sDto.setSoundFilePath(path);
+						sDto.setOwner(1);
+					}
 			} catch (Exception e) {
+				Log.d("err", e.getMessage());
 			} finally {
 				httpclient.getConnectionManager().shutdown();
 			}
@@ -100,6 +120,53 @@ public class HttpUtility {
 		    String result = EntityUtils.toString(response.getEntity());
 		    get.abort();
 		    return result;
+		}
+
+		private static boolean download(Context con,String downloadUrl , String fileName){
+			try{
+			int responseCode = 0;
+		    int BUFFER_SIZE = 10240;
+		    URI url = URI.create(downloadUrl);
+
+		    HttpClient hClient = new DefaultHttpClient();
+		    HttpGet hGet = new HttpGet();
+		    HttpResponse hResp = null;
+
+		    hClient.getParams().setParameter("http.connection.timeout", new Integer(15000));
+
+		    hGet.setURI(url);
+
+		    hResp = hClient.execute(hGet);
+
+		    responseCode = hResp.getStatusLine().getStatusCode();
+//		    String tmp = con.getCacheDir().getAbsolutePath() + "/wav.tmp";
+		    String tmp = fileName;
+		    if (responseCode == HttpStatus.SC_OK) {
+		        File file = new File(tmp);
+		        InputStream is = hResp.getEntity().getContent();
+		        BufferedInputStream in = new BufferedInputStream(is, BUFFER_SIZE);
+		        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file, false), BUFFER_SIZE);
+
+		        byte buf[] = new byte[BUFFER_SIZE];
+		        int size = -1;
+		        while((size = in.read(buf)) != -1) {
+		            out.write(buf, 0, size);
+		        }
+		        out.flush();
+
+		        out.close();
+		        in.close();
+//		        File outFIle = new File(fileName);
+//		        file.renameTo(outFIle);
+				return true;
+		    }
+		    }catch(Exception e){
+		    	Log.d("http error", e.getMessage());
+		    }finally{
+
+		    }
+
+			return false;
 		}
 
 }
